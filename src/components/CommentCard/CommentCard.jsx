2a6dc1e75ab907e942575a1c8c347b7168dc3d8e
Matheus Pereira <matheusdimmy@hotmail.com>
2023-03-27 06:41:09 -0300

    feat: Add styling to post page and comments section

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
+import Avatar from 'react-avatar';
+import './CommentCard.css';
 
-function CommentCard({ email, body }) {
+function CommentCard({ email, body, avatarColor }) {
   return (
-    <Card className="my-3">
-      <Card.Body>
-        <Card.Title>{email}</Card.Title>
-        <Card.Text>{body}</Card.Text>
-      </Card.Body>
-    </Card>
+    <div className='comment'>
+      <div className="d-flex">
+        <Avatar size="40" round name={email} color={avatarColor} />
+        <Card.Subtitle className="avatar-email">
+          <span>{email}</span>
+        </Card.Subtitle>
+      </div>
+      <Card.Text className="comment-body">{body}</Card.Text>
+    </div>
   );
 }
 
 CommentCard.propTypes = {
   email: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
+  avatarColor: PropTypes.string.isRequired,
 };
 
 export default CommentCard;
--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -4,6 +4,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { useQuery } from 'react-query';
 import { getPostById, getComments } from '../../services/api';
 import CommentCard from '../../components/CommentCard/CommentCard';
+import './PostPage.css';
 
 function PostPage() {
   const { id } = useParams();
@@ -18,19 +19,37 @@ function PostPage() {
     return <div>Loading...</div>;
   }
 
+  const randomAvatarColor = () => {
+    const colors = ['#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#795548', '#607d8b'];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+
   return (
-    <Container>
+    <Container className="post-container">
       <Row>
         <Col>
-          <h1>{post.data.title}</h1>
+          <div className="post-header">
+            <img
+              className="post-image"
+              src={`https://source.unsplash.com/random/300x200?sig=${post.data.id}`}
+              alt=""
+            />
+            <h1>{post.data.title}</h1>
+          </div>
           <p>{post.data.body}</p>
         </Col>
       </Row>
       <Row>
         <Col>
-          <h2>Comments</h2>
+          <div className="section-divider" />
+          <h2 className="comments-section">Comments</h2>
           {comments.data.map((comment) => (
-            <CommentCard key={comment.id} email={comment.email} body={comment.body} />
+            <CommentCard
+              key={comment.id}
+              email={comment.email}
+              body={comment.body}
+              avatarColor={randomAvatarColor()}
+            />
           ))}
         </Col>
       </Row>
