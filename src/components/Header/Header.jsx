007032d5e0fb6feddfd77393fd3c55a138ca3a90
Matheus Pereira <matheusdimmy@hotmail.com>
2023-03-27 02:51:29 -0300

    feat: :lipstick: style the header with a logo, navigation, and icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaUsers, FaBloggerB } from 'react-icons/fa';
+import logo from '../../assets/logo.png';
 import './Header.css';
 
 function Header() {
   return (
     <header className="header">
       <div className="container">
-        <h1 className="logo">Fake Blog</h1>
+        <h1 className="logo">
+          <img src={logo} alt="Fake Blog" />
+        </h1>
         <nav className="navigation">
           <ul>
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/">
+                <FaBloggerB style={{ color: 'black' }} className="icons"/>
+              </Link>
             </li>
             <li>
-              <Link to="/users">Usuários</Link>
+              <Link to="/users">
+                <FaUsers style={{ color: 'black' }} className="icons"/>
+              </Link>
             </li>
           </ul>
         </nav>
